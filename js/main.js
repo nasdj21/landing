09483a18d023b53703b2325966af60540320a5ad
nasdj21 <nasierra@espol.edu.ec>
2024-10-30 13:19:57 -0500

    Se realizó la guia 08

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
+const databaseURL = "https://landing-d867a-default-rtdb.firebaseio.com/coleccion.json";
+
+let sendData = () => {
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData.entries());
+
+    data["saved"] = new Date().toLocaleDateString("es-CO", {timeZone: "America/Guayaquil"})
+
+    fetch(databaseURL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Error en la solicitud: ${response.statusText}`);
+        }
+        return response.json(); // Procesa la respuesta como JSON
+    })
+    .then(result => {
+        alert('Agradeciendo tu preferencia, nos mantenemos actualizados y enfocados en atenderte como mereces'); // Maneja la respuesta con un mensaje
+        form.reset()
+    })
+    .catch(error => {
+        alert('Hemos experimentado un error. ¡Vuelve pronto!'); // Maneja el error con un mensaje
+    });
+}
+
 let ready = () => {
     console.log("DOM está listo")
 }
@@ -22,9 +52,19 @@ let loaded = (eventLoaded) => {
                     easing: "linear",
                 }
             )
+            emailElement.focus()
+
+            return;
+            
         }
+
+        sendData();
     })
+
+   
 }
+
 
 window.addEventListener("DOMContentLoaded", ready)
 window.addEventListener("load", loaded)
+
